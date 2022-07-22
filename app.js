445c09b3e1d950cd1e3167ca36a942ceb8fbab76
el19kjhl <kenluo36@gmail.com>
2022-07-22 14:23:31 +0100

    added day and night background based on user search

--- app.js
+++ app.js
@@ -4,18 +4,23 @@ const weatherui = document.querySelector("#weather-condition");
 const temperatureui = document.querySelector("#temperature");
 const card = document.querySelector("#card");
 const iconContainer = document.querySelector("#icon");
-
+const image_holder = document.querySelector("#image-holder");
 card.style.display = "none";
 const updateUI = (data) => {
     // let cityDets = data.cityDets;
     // let weather = data.weather;
     const {cityDets,weather} = data;
     
-    citynameui.textContent = cityDets.AdministrativeArea.EnglishName;
+    citynameui.textContent = cityDets.LocalizedName;
     weatherui.textContent = weather.WeatherText;
     temperatureui.textContent = weather.Temperature.Metric.Value;
     let icon_number = weather.WeatherIcon;
     iconContainer.innerHTML = `<img src="icons/${icon_number}.svg">`
+    if(weather.IsDayTime){
+        image_holder.setAttribute("src","icons/day.svg");
+    }else{
+        image_holder.setAttribute("src","icons/night.svg");    
+    }
     card.style.display = "";
 }
 const updateCity = async (city) => {
